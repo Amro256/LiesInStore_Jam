fcef52d7dd96f983e25377efc5fe26f92af69a5e
Amro <ae7630w@gre.ac.uk>
2024-11-07 19:57:31 +0000

    Added Font & Adjusted Both Scenes
    The "Help Me" font by GGBotNet is being used here

--- GameJam01/Assets/Amro Folder/Scripts/SceneManagment.cs
+++ GameJam01/Assets/Amro Folder/Scripts/SceneManagment.cs
@@ -18,7 +18,7 @@ public class SceneManagment : MonoBehaviour
         //Input for changing scene
         if(Input.GetKeyDown(KeyCode.Return)) //Return = Enter Key
         {
-            Debug.Log("Enter Key Pressed!");
+            //Debug.Log("Enter Key Pressed!"); Debugging Purposes
 
             //Change Scenes
             SceneManager.LoadScene("PrologueScene");
