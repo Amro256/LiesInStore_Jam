41678d7b5bd58ea13c23047c783201b6f55ab26d
Amro <ae7630w@gre.ac.uk>
2024-11-07 16:00:15 +0000

    Scene Swapping Done

---
+++ GameJam01/Assets/Amro Folder/Scripts/SceneManagment.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagment : MonoBehaviour
+{
+    // // Start is called before the first frame update
+    // void Start()
+    // {
+        
+    // }
+
+    //Update is called once per frame
+
+     void Update()
+     {
+        //Input for changing scene
+        if(Input.GetKeyDown(KeyCode.Return)) //Return = Enter Key
+        {
+            Debug.Log("Enter Key Pressed!");
+
+            //Change Scenes
+            SceneManager.LoadScene("PrologueScene");
+        }
+     }    
+}
