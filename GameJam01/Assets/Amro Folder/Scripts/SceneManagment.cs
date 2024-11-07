6377024b211d256e1758f33dcb0b69a7781cc9ff
Amro <ae7630w@gre.ac.uk>
2024-11-07 21:12:53 +0000

    Updated SceneManager Script
    Made the SceneManager Script a singleton and can now change scenes from one single key input

--- GameJam01/Assets/Amro Folder/Scripts/SceneManagment.cs
+++ GameJam01/Assets/Amro Folder/Scripts/SceneManagment.cs
@@ -5,23 +5,58 @@ using UnityEngine.SceneManagement;
 
 public class SceneManagment : MonoBehaviour
 {
-    // // Start is called before the first frame update
-    // void Start()
-    // {
-        
-    // }
+
+    public static SceneManagment Instance; //Singleton for global access 
+
+    //Use Void Awake to set up the 
+    private void Awake()
+    {
+        if(Instance == null)
+        {
+            Instance = this; //Set the Instance
+            DontDestroyOnLoad(gameObject); //Dont destory the game manager object when loading/swapping scenes
+        }
+        else
+        {
+            // If there's an Instance already present then destory it 
+            Destroy(gameObject);
+        }
+
+    }
 
     //Update is called once per frame
 
      void Update()
      {
+        string currentScene = SceneManager.GetActiveScene().name;
         //Input for changing scene
         if(Input.GetKeyDown(KeyCode.Return)) //Return = Enter Key
         {
             //Debug.Log("Enter Key Pressed!"); Debugging Purposes
 
-            //Change Scenes
-            SceneManager.LoadScene("PrologueScene");
+            //Check what the current scene is set to
+
+            if(currentScene == "ControlScreen")
+            {
+                //Change Scenes
+                 SceneManager.LoadScene("PrologueScene");
+            }
+            else if(currentScene == "PrologueScene")
+            {
+                SceneManager.LoadScene("Blockout");
+            }
+
+            
         }
      }    
+
+
+    //Quit game method 
+    public void QuitGame()
+    {
+        Application.Quit();
+        Debug.Log("Application Quit");
+    }
+
+
 }
